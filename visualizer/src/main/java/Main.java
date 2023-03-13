3d415c4a6dc617baf071e0d7be1d4b43f01a9d2b
Addison <chana110@mcmaster.ca>
2023-03-13 19:31:24 -0400

    Added Prof code (visualizer)

--- visualizer/src/main/java/Main.java
+++ visualizer/src/main/java/Main.java
@@ -1,52 +1,22 @@
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.GraphicRenderer;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.MeshDump;
-import ca.mcmaster.cas.se2aa4.a2.visualizer.SVGCanvas;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.configuration.Configuration;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.DebugRenderer;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.GraphicRenderer;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.SVGCanvasProvider;
+import ca.mcmaster.cas.se2aa4.a2.visualizer.renderer.Renderer;
 
 import java.awt.*;
-import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        // Extracting command line parameters
-        String input = args[0];
-        String output = args[1];
-        boolean debugMode = false;
-
-        //check for debug mode. Anything other than -X will run in Default mode
-        if(args.length == 2){
-            System.out.println("DEBUG mode OFF");
-        } else if(args.length == 3 && args[2].equals("-X")) {
-            System.out.println("DEBUG mode ON");
-            debugMode = true;
-        } else{
-            System.out.println("DEBUG mode OFF");
-        }
-
-        // Getting width and height for the canvas
-        Structs.Mesh aMesh = new MeshFactory().read(input);
-        double max_x = Double.MIN_VALUE;
-        double max_y = Double.MIN_VALUE;
-        for (Structs.Vertex v: aMesh.getVerticesList()) {
-            max_x = (Double.compare(max_x, v.getX()) < 0? v.getX(): max_x);
-            max_y = (Double.compare(max_y, v.getY()) < 0? v.getY(): max_y);
-        }
-
-        // Creating the Canvas to draw the mesh
-        Graphics2D canvas = SVGCanvas.build((int) Math.ceil(max_x), (int) Math.ceil(max_y));
-
-        GraphicRenderer renderer = new GraphicRenderer();
-
-        // Painting the mesh on the canvas. Added debug mode parameter to switch it on or off
-        renderer.render(aMesh, canvas, debugMode);
-        // Storing the result in an SVG file
-        SVGCanvas.write(canvas, output);
-        // Dump the mesh to stdout
-        MeshDump dumper = new MeshDump();
-        dumper.dump(aMesh);
-
-
+    public static void main(String[] args) throws Exception {
+        Configuration config = new Configuration(args);
+        Structs.Mesh aMesh = new MeshFactory().read(config.input());
+        Graphics2D canvas = SVGCanvasProvider.build(aMesh);
+        Renderer renderer = (config.debug()? new DebugRenderer(): new GraphicRenderer());
+        renderer.render(aMesh, canvas);
+        SVGCanvasProvider.write(canvas, config.output());
     }
+
 }
