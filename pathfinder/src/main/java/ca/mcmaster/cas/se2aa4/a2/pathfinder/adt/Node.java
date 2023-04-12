44e30aaa9c3026c3b8afbaec39ccdea10ffc336c
Hemraj <bhatth14@mcmaster.ca>
2023-04-12 12:16:51 -0400

    F03

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a2/pathfinder/adt/Node.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a2/pathfinder/adt/Node.java
@@ -1,10 +1,45 @@
 package ca.mcmaster.cas.se2aa4.a2.pathfinder.adt;
 
-import ca.mcmaster.cas.se2aa4.a2.io.Structs;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
+
+    private final String id;
+    private final Edge segment;
+    private final List<Edge> edges;
+    private String cName;
+
+    public Node(String id, Edge seg, List<Edge> edges) {
+        this.id = id;
+        this.segment = seg;
+        this.edges=edges;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Edge getSegment(){
+        return segment;
+    }
+
+   public List<Edge> getEdges(){
+       return edges;
+   }
+
+   public void edgeAdd(Node end,int w){
+        Edge edge = new Edge(this,end,w); // creates new edge
+       edges.add(edge);
+   }
+
+    public String getCityNode(){ // provides a edge
+        return  cName;
+    }
+
+    public void setCityNode(String x){ // sets an edge
+        this.cName = x;
+    }
+
 
 }
